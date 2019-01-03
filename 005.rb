09338a5ad913c5bf155c5235f441817442e0f42d
Robert Fisher <services@id264.net>
2019-01-03 19:12:02 +0000

    linting

--- 005.rb
+++ 005.rb
@@ -5,7 +5,7 @@ R = [19, 17, 16, 15, 14, 13, 12, 11, 18, 20].freeze
 x = 2520
 
 loop do
-  break unless R.any? { |n| (x % n) > 0 }
+  break unless R.any? { |n| (x % n).positive? }
   x += 2 # the answer can't be odd
 end
 
--- 016.rb
+++ 016.rb
@@ -1,3 +1,5 @@
 #!/usr/bin/env ruby
 
+# rubocop:disable Lint/ParenthesesAsGroupedExpression
 puts (2**1000).to_s.split('').map(&:to_i).inject(:+)
+# rubocop:enable Lint/ParenthesesAsGroupedExpression
