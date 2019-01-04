60384cb76aa29c7292621ad45fe239dfae21bcaf
Robert Fisher <services@id264.net>
2019-01-04 01:28:33 +0000

    one more before bed

---
+++ 022.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require 'pathname'
+
+ALPHABET = ('@'..'Z').to_a.freeze
+
+def name2score(name)
+  name.each_char.map { |l| ALPHABET.index(l) }.inject(:+)
+end
+
+f = IO.read(Pathname.new(__FILE__).dirname + 'resources' + 'p022_names.txt')
+list = f.delete('"').split(',').sort
+sum = 0
+list.each.with_index(1) { |name, i| sum += i * name2score(name) }
+puts sum
