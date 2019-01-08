ff12136f0cc84f305e8f393c10ce605862feaf39
Robert Fisher <services@id264.net>
2019-01-08 22:41:27 +0000

    implemented someone else's solution for 24

---
+++ 024.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+# I gave up on this one. Couldn't quite nail the algorithm for the
+# next lexicograpic number. Learnt more about combinatorics digging
+# for the solution, and in this exercise, new knowledge gained >>
+# old knowledge applied. (I was on to the right way of doing it,
+# just didn't know quite enough.). The solution is to chop away n!
+# chunks of the million, moving along the number.
+
+require_relative 'lib/common'
+
+numbers = ('0'..'9').to_a
+N = numbers.size
+perm_num = ''
+remain = 1_000_000 - 1
+
+1.upto(N) do |i|
+  j = remain / (N - i).factorial
+  remain = remain % (N - i).factorial
+  perm_num.<< numbers[j].to_s
+  numbers.delete_at(j)
+  break if remain.zero?
+end
+
+puts perm_num + numbers.join
