51e311ad05f33f28a4995b6d2a0d7109e254f7e9
Robert Fisher <services@id264.net>
2019-01-08 18:03:01 +0000

    move common methods to lib

--- 021.rb
+++ 021.rb
@@ -1,24 +1,14 @@
 #!/usr/bin/env ruby
 
-def proper_divisors(n)
-  (1..n**0.5).select { |d| (n % d).zero? }.each_with_object([]) do |d, a|
-    a.<< d
-    d2 = n / d
-    a.<< d2 unless d2 == d || d2 == n
-  end.sort
-end
-
-def d_of_n(n)
-  proper_divisors(n).inject(:+)
-end
+require_relative 'lib/common'
 
 MAX = 10_000
 sum = 0
 
 (1..MAX).each do |n|
-  d1 = d_of_n(n)
+  d1 = sum_of_proper_divisors(n)
   next unless d1 > n && d1 <= MAX
-  d2 = d_of_n(d1)
+  d2 = sum_of_proper_divisors(d1)
   sum += n + d1 if d2 == n
 end
 
---
+++ lib/common.rb
@@ -0,0 +1,13 @@
+# Methods which get used in multiple problems
+
+def proper_divisors(n)
+  (1..n**0.5).select { |d| (n % d).zero? }.each_with_object([]) do |d, a|
+    a.<< d
+    d2 = n / d
+    a.<< d2 unless d2 == d || d2 == n
+  end.sort
+end
+
+def sum_of_proper_divisors(n)
+  proper_divisors(n).inject(:+)
+end
