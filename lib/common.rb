965483222362f2f2cf535ba6301c74f85a2f8369
Robert Fisher <services@id264.net>
2019-01-09 17:06:27 +0000

    access a predefined table of primes

--- lib/common.rb
+++ lib/common.rb
@@ -1,4 +1,8 @@
 # Methods which get used in multiple problems
+#
+require 'pathname'
+
+RESOURCE_DIR = Pathname.new(__FILE__).dirname.parent + 'resources'
 
 def proper_divisors(n)
   (1..n**0.5).select { |d| (n % d).zero? }.each_with_object([]) do |d, a|
@@ -22,4 +26,22 @@ class Numeric
       nil
     end
   end
+
+  # requires the prime numbers loaded into PRIMES
+  #
+  def prime?
+    PRIMES[self]
+  end
+end
+
+# Load primes into a numberline (i.e. an array of true/false). This
+# is much quicker to check than an array of the primes themselves.
+#
+def load_primes
+  p = IO.read(RESOURCE_DIR + 'primes.txt').each_line.map { |n| n.to_i }
+  ([false] * p.max).tap { |nl| p.each { |n| nl[n] = true } }
+end
+
+def load_primes_as_arr
+  IO.read(RESOURCE_DIR + 'primes.txt').each_line.map { |n| n.to_i }
 end
