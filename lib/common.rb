09d465d95289c4784e8915c8de2129ae8caabddb
Robert Fisher <services@id264.net>
2019-01-09 17:08:19 +0000

    linting

--- lib/common.rb
+++ lib/common.rb
@@ -16,17 +16,21 @@ def sum_of_proper_divisors(n)
   proper_divisors(n).inject(:+)
 end
 
+# Convenience methods for numbers
+#
 class Numeric
   def factorial
-    if self.zero?
+    if zero?
       1
-    elsif self.positive?
+    elsif positive?
       1.upto(self).inject(:*)
-    else
-      nil
     end
   end
+end
 
+# Convenience methods for integers
+#
+class Integer
   # requires the prime numbers loaded into PRIMES
   #
   def prime?
@@ -38,10 +42,10 @@ end
 # is much quicker to check than an array of the primes themselves.
 #
 def load_primes
-  p = IO.read(RESOURCE_DIR + 'primes.txt').each_line.map { |n| n.to_i }
+  p = IO.read(RESOURCE_DIR + 'primes.txt').each_line.map(&:to_i)
   ([false] * p.max).tap { |nl| p.each { |n| nl[n] = true } }
 end
 
 def load_primes_as_arr
-  IO.read(RESOURCE_DIR + 'primes.txt').each_line.map { |n| n.to_i }
+  IO.read(RESOURCE_DIR + 'primes.txt').each_line.map(&:to_i)
 end
