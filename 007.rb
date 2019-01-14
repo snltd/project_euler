3ad90348ecea837c0d978b2e7f678a193e318ecc
Robert Fisher <services@id264.net>
2019-01-14 12:59:23 +0000

    fix syntax error

--- 007.rb
+++ 007.rb
@@ -3,7 +3,7 @@
 def primes_upto(max)
   (0..max).to_a.tap do |line|
     (2..(max**0.5).round + 1).each do |n|
-      (2 * n..max).step(n) { |v| line[v] = nil } if line[v]
+      (2 * n..max).step(n) { |v| line[v] = nil if line[v] }
     end
   end.drop(2).reject(&:nil?)
 end
