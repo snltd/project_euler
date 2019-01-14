4ec553b81f2575a96b8d6e87c261a01a0298eecf
Robert Fisher <services@id264.net>
2019-01-14 13:02:12 +0000

    bugfix

--- 010.rb
+++ 010.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
 def primes_upto(max)
-  (0..max).to_a.tap do |line|
+  (0..max).lazy.to_a.tap do |line|
     (2..(max**0.5).round + 1).each do |n|
-      (2 * n..max).step(n) { |v| line[v] = nil } if line[v]
+      (2 * n..max).step(n) { |v| line[v] = nil if line[v] }
     end
   end.drop(2).reject(&:nil?)
 end
