de481095ac21315fc51be66e617c1154e1d923fd
Robert Fisher <services@id264.net>
2019-10-15 23:51:52 +0100

    much simpler solution

--- ruby/001.rb
+++ ruby/001.rb
@@ -1,17 +1,15 @@
 #!/usr/bin/env ruby
+#
+# Multiples of 3 and 5
+# Problem 1
+#
+# If we list all the natural numbers below 10 that are multiples of
+# 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#
+# Find the sum of all the multiples of 3 or 5 below 1000.
 
 require 'set'
 
-LIMIT = 1000
-
-def multiples_of(*int)
-  int.each_with_object(Set.new) do |n, a|
-    i = n
-    while i < LIMIT
-      a.<< i
-      i += n
-    end
-  end
-end
-
-puts multiples_of(3, 5).sum
+threes = Set.new((0...1000).step(3))
+fives  = Set.new((0...1000).step(5))
+puts (threes + fives).sum
