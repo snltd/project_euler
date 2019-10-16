5cb68e38d68ce613df367deed486e16bdf8b81c3
Robert Fisher <services@id264.net>
2019-10-16 10:31:43 +0100

    whitespace

--- ruby/028.rb
+++ ruby/028.rb
@@ -6,10 +6,9 @@ def diagonal_sum(eqn, limit = 500)
   1.upto(limit).map { |n| eqn.call(n) }.sum
 end
 
-
 diagonals = [->(n) { (n * 2 + 1)**2 }, # up-right
              ->(n) { (n * 2)**2 - 2 * n + 1 }, # down-right
-             ->(n) { (n * 2)**2 + 1 },
-             ->(n) { (n * 2)**2 + 2 * n + 1 }]
+             ->(n) { (n * 2)**2 + 1 }, #down-left
+             ->(n) { (n * 2)**2 + 2 * n + 1 }] # up-left
 
 puts diagonals.map { |d| diagonal_sum(d) }.sum + 1
